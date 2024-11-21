cb4f3a774bbab62daaf9e1da86e56b981fff68b7
HenryKlinck1 <hklinck@cps.edu>
2024-11-21 10:37:31 -0600

    thurs

--- a5.py
+++ a5.py
@@ -159,9 +159,7 @@ class Board:
         self.num_nums_placed += 1
 
         for i in range(self.size):
-            # remove the assignment from the row
             remove_if_exists(self.rows[row][i], assignment)
-            # remove the assignment from the column
             remove_if_exists(self.rows[i][column], assignment)
 
         for i, j in self.subgrid_coordinates(row, column):
@@ -180,39 +178,22 @@ def DFS(state: Board) -> Board:
         either None in the case of invalid input or a solved board
     """
     s = Stack([state])
+    num = 0
 
     while not s.is_empty():
-        print(s)
         b: Board = s.pop()
-        # print(b)
+        num += 1
         if b.goal_test():
+            print(f"Number of Iterations: {num}")
             return b
         mcc = b.find_most_constrained_cell()
-        print(mcc)
         
         row = mcc[0]
         col = mcc[1]
-        print(b.rows[row][col])
-        print()
         for sel in b.rows[row][col]:
             cpy = copy.deepcopy(b)
             cpy.update(row, col, sel)
             s.push(cpy)
-
-    # print(s)
-    # b = s.pop()
-    # b.print_pretty()
-    # print(b)
-    # mcc = b.find_most_constrained_cell()
-    # print(mcc)
-    
-    # b.update(mcc[0], mcc[1], b.rows[mcc[0]][mcc[1]][0])
-    # s.push(b)
-    
-    # print(s)
-    # b = s.pop()
-    # b.print_pretty()
-
 
 def BFS(state: Board) -> Board:
     """Performs a breadth first search. Takes a Board and attempts to assign values to
@@ -226,7 +207,23 @@ def BFS(state: Board) -> Board:
     Returns:
         either None in the case of invalid input or a solved board
     """
-    pass
+    q = Queue([state])
+    num = 0
+
+    while not q.is_empty():
+        b: Board = q.pop()
+        num += 1
+        if b.goal_test():
+            print(f"Number of Iterations: {num}")
+            return b
+        mcc = b.find_most_constrained_cell()
+        
+        row = mcc[0]
+        col = mcc[1]
+        for sel in b.rows[row][col]:
+            cpy = copy.deepcopy(b)
+            cpy.update(row, col, sel)
+            q.push(cpy)
 
 
 if __name__ == "__main__":
@@ -266,65 +263,60 @@ if __name__ == "__main__":
 
     # sets of moves for the different games
     first_moves = [
-        (0, 1, 7),
-        (0, 7, 1),
-        (1, 2, 9),
-        (1, 3, 7),
-        (1, 5, 4),
-        (1, 6, 2),
+        (0, 0, 3),
+        (0, 4, 8),
+        (0, 6, 9),
+        (1, 3, 3),
+        (1, 4, 4),
         (2, 2, 8),
-        (2, 3, 9),
-        (2, 6, 3),
-        (3, 1, 4),
-        (3, 2, 3),
-        (3, 4, 6),
-        (4, 1, 9),
-        (4, 3, 1),
-        (4, 5, 8),
-        (4, 7, 7),
-        (5, 4, 2),
-        (5, 6, 1),
-        (5, 7, 5),
-        (6, 2, 4),
-        (6, 5, 5),
-        (6, 6, 7),
-        (7, 2, 7),
-        (7, 3, 4),
-        (7, 5, 1),
-        (7, 6, 9),
-        (8, 1, 3),
-        (8, 7, 8),
+        (2, 5, 5),
+        (2, 6, 6),
+        (3, 0, 5),
+        (3, 3, 1),
+        (3, 5, 4),
+        (3, 7, 7),
+        (4, 2, 2),
+        (4, 5, 9),
+        (4, 7, 1),
+        (5, 2, 3),
+        (5, 7, 4),
+        (6, 2, 5),
+        (6, 5, 1),
+        (6, 6, 2),
+        (8, 1, 7),
+        (8, 5, 8),
+        (8, 7, 9),
     ]
 
-    second_moves = [
-        (0, 1, 2),
-        (0, 3, 3),
-        (0, 5, 5),
-        (0, 7, 4),
-        (1, 6, 9),
-        (2, 1, 7),
-        (2, 4, 4),
-        (2, 7, 8),
-        (3, 0, 1),
-        (3, 2, 7),
-        (3, 5, 9),
-        (3, 8, 2),
-        (4, 1, 9),
-        (4, 4, 3),
-        (4, 7, 6),
-        (5, 0, 6),
-        (5, 3, 7),
-        (5, 6, 5),
-        (5, 8, 8),
-        (6, 1, 1),
-        (6, 4, 9),
-        (6, 7, 2),
-        (7, 2, 6),
-        (8, 1, 4),
-        (8, 3, 8),
-        (8, 5, 7),
-        (8, 7, 5),
-    ]
+    # second_moves = [
+    #     (0, 1, 2),
+    #     (0, 3, 3),
+    #     (0, 5, 5),
+    #     (0, 7, 4),
+    #     (1, 6, 9),
+    #     (2, 1, 7),
+    #     (2, 4, 4),
+    #     (2, 7, 8),
+    #     (3, 0, 1),
+    #     (3, 2, 7),
+    #     (3, 5, 9),
+    #     (3, 8, 2),
+    #     (4, 1, 9),
+    #     (4, 4, 3),
+    #     (4, 7, 6),
+    #     (5, 0, 6),
+    #     (5, 3, 7),
+    #     (5, 6, 5),
+    #     (5, 8, 8),
+    #     (6, 1, 1),
+    #     (6, 4, 9),
+    #     (6, 7, 2),
+    #     (7, 2, 6),
+    #     (8, 1, 4),
+    #     (8, 3, 8),
+    #     (8, 5, 7),
+    #     (8, 7, 5),
+    # ]
     #Create a sudoku board.
     b = Board()
     #Place the 28 assignments in first_moves on the board.
@@ -391,11 +383,11 @@ if __name__ == "__main__":
 
     test_dfs_or_bfs(True, second_moves)
 
-    # print("<<<<<<<<<<<<<< Testing BFS on First Game >>>>>>>>>>>>>>")
+    print("<<<<<<<<<<<<<< Testing BFS on First Game >>>>>>>>>>>>>>")
 
-    # test_dfs_or_bfs(False, first_moves)
+    test_dfs_or_bfs(False, first_moves)
 
-    # print("<<<<<<<<<<<<<< Testing BFS on Second Game >>>>>>>>>>>>>>")
+    print("<<<<<<<<<<<<<< Testing BFS on Second Game >>>>>>>>>>>>>>")
 
-    # test_dfs_or_bfs(False, second_moves)
+    test_dfs_or_bfs(False, second_moves)
     pass
